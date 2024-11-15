0b79eb0f8a97d7e29a67c7504a48fc6535d073d9
Praharsha <Praharshajannu@gmail.com>
2024-11-15 15:37:05 +0530

    created java maven application.

---
+++ src/main/java/com/maven/demo/javaExample/App.java
@@ -0,0 +1,10 @@
+package com.maven.demo.javaExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("praharsha");
+    }
+}
